ce1e42b9dd0d2fb1588dff95e3a5b0bf7fdea5dc
4mbk0r <4mbk0r@gmail.com>
2022-08-15 09:44:50 -0400

    noda en heroku

--- Projecto1/settings.py
+++ Projecto1/settings.py
@@ -174,6 +174,7 @@ DATABASES['default'].update(db_from_env)
 ###    os.path.join(BASE_DIR, 'static'),
 ###)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
