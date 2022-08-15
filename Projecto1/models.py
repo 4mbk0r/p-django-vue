f2dd4e3f260460fa78b5948de13c2cfc96eaba6e
4mbk0r <4mbk0r@gmail.com>
2022-08-15 07:11:53 -0400

    esto es el inicio23s

--- Projecto1/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
- 
-# model named Post
-class Model_Login(models.Model):
-    nombre = models.CharField(max_length=100)
-    clave = models.CharField( max_length=100)
-class Index(models.Model):
-    titulo = models.CharField(max_length=100)
-    nombre = models.TextField(max_length=200)
